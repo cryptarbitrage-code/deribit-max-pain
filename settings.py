4208b44a6a09cde4ac8a3782f13f6c62c5b4cd6f
cryptarbitrage-code <cryptosheetsharing+github@gmail.com>
2022-05-16 14:08:44 +0100

    switch settings to production website

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
 # Account and api details are not shared between test and production sites. Be sure to enter the correct url.
-api_exchange_address = "https://test.deribit.com"
+api_exchange_address = "https://www.deribit.com"
